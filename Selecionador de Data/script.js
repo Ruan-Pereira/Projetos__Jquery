abb0c39f115d2353a9d10f55fb679eae1749b31e
Ruan-Pereira <ruanborbapereira@gmail.com>
2020-12-20 15:55:33 -0300

    Selecionador de data finalizado

---
+++ Selecionador de Data/script.js
@@ -0,0 +1,31 @@
+$(function(){
+   
+    var horaescolhaCampo;
+
+    $('input').on('focus', function(){
+        
+        var pos = $(this).offset();
+        var width = $(this).width();
+
+        $('.horaescolha').css('left', pos.left + width + 8)
+        $('.horaescolha').css('top', pos.top)
+        $('.horaescolha').show();
+
+        horaescolhaCampo = $(this);
+
+    });
+
+    $('input').on('blur', function(){
+        setTimeout(function(){
+            $('.horaescolha').hide();
+        }, 200);
+    });
+
+    $('.horaescolha button').on('click', function(){
+         
+        var hora = $(this).html();
+        horaescolhaCampo.val(hora);
+    })
+
+
+})
